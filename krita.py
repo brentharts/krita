5a2dff9f1c0201ffd0347f036aaf3c576c12146a
brent hartshorn <brenthartshorn@proton.me>
2024-09-05 19:53:53 -0700

    libunibreak-dev

--- krita.py
+++ krita.py
@@ -5,7 +5,7 @@ if not os.path.isdir('./build'):
 
 if '--install' in sys.argv:
 	#os.system('sudo apt-get build-dep krita')
-	os.system('sudo apt-get install libraqm-dev libexiv2-dev libquazip5-dev libmypaint-dev libqt5svg5-dev libkf5itemmodels-dev libkf5config-dev libkdecorations2-dev libqt5x11extras5-dev qtdeclarative5-dev libkf5guiaddons-dev libkf5configwidgets-dev libkf5windowsystem-dev libkf5coreaddons-dev')
+	os.system('sudo apt-get install libunibreak-dev libraqm-dev libexiv2-dev libquazip5-dev libmypaint-dev libqt5svg5-dev libkf5itemmodels-dev libkf5config-dev libkdecorations2-dev libqt5x11extras5-dev qtdeclarative5-dev libkf5guiaddons-dev libkf5configwidgets-dev libkf5windowsystem-dev libkf5coreaddons-dev')
 
 
 cmd = ['cmake', os.path.abspath('.')]
