d82f1c40535d6c4b0706bf7db6d9ab268288e7ef
brent hartshorn <brenthartshorn@proton.me>
2024-09-05 17:57:40 -0700

    krita build front end

---
+++ krita.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os, sys, subprocess
+if not os.path.isdir('./build'):
+	os.mkdir('./build')
+
+if '--install' in sys.argv:
+	#os.system('sudo apt-get build-dep krita')
+	os.system('sudo apt-get install libquazip5-dev libmypaint-dev libqt5svg5-dev libkf5itemmodels-dev libkf5config-dev libkdecorations2-dev libqt5x11extras5-dev qtdeclarative5-dev libkf5guiaddons-dev libkf5configwidgets-dev libkf5windowsystem-dev libkf5coreaddons-dev')
+
+
+cmd = ['cmake', os.path.abspath('.')]
+print(cmd)
+subprocess.check_call(cmd, cwd='./build')
